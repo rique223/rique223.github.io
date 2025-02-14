6108be4bf33ce99a3c66850a9e4269a408a43ccd
rique223 <henrique.jobs1@gmail.com>
2025-02-14 17:30:55 -0300

    refactor: :recycle: Improve terminal styling

--- app/Components/Terminal.tsx
+++ app/Components/Terminal.tsx
@@ -18,21 +18,25 @@ const Terminal: React.FC<TerminalProps> = ({ input, setInput, output, handleComm
 	};
 
 	return (
-		<div className={`h-[87vh] container mx-auto p-4 font-mono ${darkMode ? 'bg-gray-800 text-green-400' : 'bg-white text-gray-900'} flex flex-col rounded`}>
-			<div ref={terminalRef} className='flex-1 overflow-y-auto mb-4 p-2'>
+		<div
+			className={`h-[65vh] container mx-auto font-mono ${
+				darkMode ? 'bg-black text-green-400' : 'bg-white text-gray-900'
+			} flex flex-col rounded-lg border border-[#3E3E3E]`}
+		>
+			<div ref={terminalRef} className='flex-1 overflow-y-auto p-4'>
 				{output.map((line, index) => (
 					<div key={index} className='whitespace-pre-wrap'>
 						{line}
 					</div>
 				))}
 			</div>
-			<form onSubmit={handleSubmit} className='flex'>
-				<span className='mr-2'>{currentDirectory}$</span>
+			<form onSubmit={handleSubmit} className='flex border-t border-[#191919] p-4'>
+				<span className='mr-2 text-[#FF8FFD]'>{currentDirectory}</span>
 				<input
 					type='text'
 					value={input}
 					onChange={(e) => setInput(e.target.value)}
-					className={`flex-grow outline-none ${darkMode ? 'bg-gray-800 text-green-400' : 'bg-white text-gray-900'}`}
+					className={`flex-grow outline-none ${darkMode ? 'bg-black text-[#E5E5E5]' : 'bg-white text-gray-900'} text-[13px]`}
 					autoFocus
 				/>
 			</form>
